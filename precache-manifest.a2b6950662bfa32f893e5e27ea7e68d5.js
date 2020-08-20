28376e7162bdbfce784e41ec7df3e363ea9fa490
Jonathan <tangjon@hotmail.com>
2020-08-19 22:44:07 -0700

    Updates

---
+++ precache-manifest.a2b6950662bfa32f893e5e27ea7e68d5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96aa5034640072f2996e02643267933e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61b1a39746cd7688e9c0",
+    "url": "/static/css/main.e5e7f7a2.chunk.css"
+  },
+  {
+    "revision": "1307bc638b77992f2cc6",
+    "url": "/static/js/2.e8e409c9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.e8e409c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61b1a39746cd7688e9c0",
+    "url": "/static/js/main.4b60812b.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "1a632d2b39f3275752f2c4b3c73477a3",
+    "url": "/static/media/angular.1a632d2b.png"
+  },
+  {
+    "revision": "2f07f54517b2d8f500fea6e91dc8feac",
+    "url": "/static/media/bapc.2f07f545.png"
+  },
+  {
+    "revision": "64aaeb1642cfcfb42a075db1fe9994da",
+    "url": "/static/media/bootstrap.64aaeb16.png"
+  },
+  {
+    "revision": "68590cbfac435e659b8000731dd2e21a",
+    "url": "/static/media/carbon-tracker.68590cbf.png"
+  },
+  {
+    "revision": "1d528e2cb5fbc29ad16c74e5d883c371",
+    "url": "/static/media/django.1d528e2c.png"
+  },
+  {
+    "revision": "b0f7359f780365e42ebd7e73cb36541f",
+    "url": "/static/media/headshot.b0f7359f.jpg"
+  },
+  {
+    "revision": "1781432ed8370cf4cadb074fa9bc69a9",
+    "url": "/static/media/mysql.1781432e.jpg"
+  },
+  {
+    "revision": "dc199ecc9e90e4bc7e9ed9b31e3c2b5f",
+    "url": "/static/media/python.dc199ecc.png"
+  },
+  {
+    "revision": "00f6152a2b51116a4725fbb0a7d3949e",
+    "url": "/static/media/qmcd.00f6152a.png"
+  },
+  {
+    "revision": "7c0fdc490914ad5e54763db4ef955066",
+    "url": "/static/media/react.7c0fdc49.png"
+  },
+  {
+    "revision": "bd705acd5e64f6d4aa8bf70ee760364a",
+    "url": "/static/media/sap.bd705acd.png"
+  },
+  {
+    "revision": "accafba0e22a4f33f948811221f09537",
+    "url": "/static/media/wordpress.accafba0.png"
+  }
+]);
